8f6312c704bb5d621877cd470b3b1bc3837d209d
Garnold02 <gulyas.arnold02@gmail.com>
2019-04-03 21:48:10 +0200

    Fixed lighting bug

--- LudumGL/Shaders.cs
+++ LudumGL/Shaders.cs
@@ -56,23 +56,38 @@ uniform Light lights[10];
 
 void main() {
     vec4 combinedLightColor=vec4(0,0,0,1);
-    vec4 worldPos=projection*translation*rotation*scale*localPos;
+    vec4 worldPos=translation*rotation*scale*localPos;
     for(int i=0; i<10; i++) {
         Light light=lights[i];
         if(light.enabled==0) continue;
         vec4 lightPos=light.translation*vec4(0,0,0,1);
         vec3 normalRot=(rotation*normal).xyz;
-        vec3 lightConnection=worldPos.xyz-lightPos.xyz;
+        vec3 lightConnection=lightPos.xyz - worldPos.xyz;
         vec3 lightDirection=normalize(lightConnection);
         float lightDistance=length(lightConnection);
 
         float rangeMod=1-clamp(lightDistance/light.range,0,1);
-        float diffuse=max(dot(lightDirection, normalRot), 0.0);
+        float diffuse=dot(lightDirection,normalRot)/lightDistance;
 
-        vec4 lightColor=light.color*rangeMod*diffuse*light.color.w;
+        vec4 lightColor=light.color*diffuse*rangeMod*light.color.w;
         combinedLightColor+=lightColor;
     }
     FragColor=vec4(combinedLightColor.xyz, 1.0);
+}
+
+        ";
+        public static string Unlit { get; } = @"
+
+#version 440
+
+out vec4 FragColor;
+
+in vec4 localPos;
+in vec4 normal;
+in vec4 uv;
+
+void main() {
+    FragColor=vec4(1,1,1,1);
 }
 
         ";
