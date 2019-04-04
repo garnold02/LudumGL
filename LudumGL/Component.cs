db70b0c4b6e88c6a0a76a381ad846add67e2d1c0
Garnold02 <gulyas.arnold02@gmail.com>
2019-04-04 22:10:01 +0200

    Incomplete implementation

--- LudumGL/Component.cs
+++ LudumGL/Component.cs
@@ -6,18 +6,32 @@ namespace LudumGL
     /// Base class for customized components that can be
     /// attached to GameObjects.
     /// </summary>
-    abstract class Component
+    public abstract class Component
     {
+        bool internalEnabled;
+
+        public bool Enabled
+        {
+            get => internalEnabled;
+            set
+            {
+                internalEnabled = value;
+                if (internalEnabled)
+                    OnEnable();
+                else
+                    OnDisable();
+            }
+        }
         /// <summary>
         /// The parent GameObject of this Component.
         /// </summary>
-        public GameObject Parent { get; }
+        public GameObject Parent { get; internal set; }
 
         /// <summary>
         /// Runs when the component is added to a
         /// GameObject.
         /// </summary>
-        public virtual void Start()
+        internal virtual void Start()
         {
 
         }
@@ -25,7 +39,39 @@ namespace LudumGL
         /// <summary>
         /// Runs every frame when this Component is enabled.
         /// </summary>
-        public virtual void Update()
+        internal virtual void Update()
+        {
+
+        }
+
+        /// <summary>
+        /// Runs once the parent GameObject becomes enabled.
+        /// </summary>
+        public virtual void OnParentEnable()
+        {
+
+        }
+
+        /// <summary>
+        /// Runs once the parent GameObject becomes disabled.
+        /// </summary>
+        public virtual void OnParentDisable()
+        {
+
+        }
+
+        /// <summary>
+        /// Runs once the component becomes enabled.
+        /// </summary>
+        public virtual void OnEnable()
+        {
+
+        }
+
+        /// <summary>
+        /// Runs once the component becomes disabled.
+        /// </summary>
+        public virtual void OnDisable()
         {
 
         }
--- LudumGL/GameObject.cs
+++ LudumGL/GameObject.cs
@@ -1,21 +1,126 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 namespace LudumGL
 {
     /// <summary>
     /// GameObject implementation.
     /// </summary>
-    class GameObject
+    public class GameObject
     {
+        #region Static
+        internal static List<GameObject> gameObjects = new List<GameObject>();
+        internal static List<GameObject> gameObjectsAdd = new List<GameObject>();
+        internal static List<GameObject> gameObjectsRemove = new List<GameObject>();
+
+        internal static void Update()
+        {
+            foreach (GameObject gameObject in gameObjects)
+            {
+                gameObject.drawable.transform = gameObject.transform;
+                gameObject.UpdateComponents();
+            }
+
+            gameObjects.AddRange(gameObjectsAdd);
+            foreach (GameObject gameObject in gameObjectsRemove)
+            {
+                gameObjects.Remove(gameObject);
+            }
+
+            gameObjectsAdd.Clear();
+            gameObjectsRemove.Clear();
+        }
+        internal static void Render()
+        {
+            foreach (GameObject gameObject in gameObjects)
+            {
+                if(gameObject.drawable!=null)
+                {
+                    gameObject.drawable.Render(Game.mainCamera);
+                }
+            }
+        }
+        /// <summary>
+        /// Adds a GameObject to the system.
+        /// </summary>
+        /// <param name="gameObject"></param>
+        public static void Add(GameObject gameObject)
+        {
+            gameObjectsAdd.Add(gameObject);
+        }
+
+        /// <summary>
+        /// Destroys a GameObject.
+        /// </summary>
+        /// <param name="gameObject"></param>
+        public static void Destroy(GameObject gameObject)
+        {
+            gameObjectsRemove.Add(gameObject);
+        }
+        #endregion
+
+        bool internalEnabled;
+        readonly List<Component> components;
+
+        public bool Enabled
+        {
+            get => internalEnabled;
+            set
+            {
+                internalEnabled = value;
+                if(internalEnabled)
+                {
+                    foreach (Component component in components)
+                    {
+                        component.OnParentEnable();
+                    }
+                }
+                else
+                {
+                    foreach (Component component in components)
+                    {
+                        component.OnParentDisable();
+                    }
+                }
+            }
+        }
+
         /// <summary>
         /// The transform of this GameObject.
         /// </summary>
-        public Transform transform;
+        public Transform transform=Transform.Identity;
+
+        /// <summary>
+        /// The drawable of this GameObject. Contains the shaders, texture, and mesh
+        /// neccessary for drawing.
+        /// </summary>
+        public Drawable drawable;
 
         public GameObject()
         {
+            components = new List<Component>();
             transform = Transform.Identity;
+        }
+
+        /// <summary>
+        /// Adds a Component to the GameObject and calls its
+        /// start function.
+        /// </summary>
+        /// <param name="component"></param>
+        public void AddComponent(Component component)
+        {
+            components.Add(component);
+            component.Parent = this;
+            component.Start();
+        }
+
+        internal void UpdateComponents()
+        {
+            foreach (Component component in components)
+            {
+                if (component.Enabled)
+                    component.Update();
+            }
         }
     }
 }
