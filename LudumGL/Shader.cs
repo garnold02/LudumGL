ff7de503e458a3d445e34794eb8e3fe1d5b505c6
Garnold02 <gulyas.arnold02@gmail.com>
2019-04-04 22:10:23 +0200

    Added for easier use

---
+++ LudumGL/Shader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using OpenTK.Graphics.OpenGL;
+
+namespace LudumGL
+{
+    public class Shader
+    {
+        public string Code { get; set; }
+        public ShaderType Type { get; }
+        public Shader(string from, bool raw, ShaderType type)
+        {
+            Type = type;
+            if(raw)
+            {
+                Code = from;
+            }
+            else
+            {
+                Code = File.ReadAllText(from);
+            }
+        }
+    }
+}
