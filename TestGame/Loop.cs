3ffa0c54be24911970a310ff1c01d0841a8bd3a2
Garnold02 <gulyas.arnold02@gmail.com>
2019-04-04 22:10:41 +0200

    Modified for clarity

--- TestGame/Loop.cs
+++ TestGame/Loop.cs
@@ -8,76 +8,80 @@ namespace TestGame
 {
     class Loop : GameLoop
     {
-        static Drawable drawable;
+        static Light light;
+        static Camera camera;
+        static GameObject cube;
+        static GameObject monkey;
 
         public override void Start()
         {
-            drawable = new Drawable
-            {
-                mesh = Mesh.Load("assets/mesh/monkey.dae"),
-                transform = new Transform() { position = new Vector3(0, 0, 0) }
-            };
-            drawable.AddShaderRaw(Shaders.Projection, OpenTK.Graphics.OpenGL.ShaderType.VertexShader);
-            drawable.AddShaderRaw(Shaders.Lit, OpenTK.Graphics.OpenGL.ShaderType.FragmentShader);
-            drawable.Finish();
-
-            Light light1 = new Light
+            light = new Light
             {
                 enabled = true,
-                color = new Vector4(1, 0.8f, 1, 2),
-                range = 10,
+                type=LightType.Point,
+                color = new Vector4(1, 1, 1, 1),
+                range = 30,
                 position = new Vector3(0, 0, 5),
                 rotation = Quaternion.FromEulerAngles(0, 0, 0)
             };
-            Light light2 = new Light
-            {
-                enabled = true,
-                color = new Vector4(0, 1, 0, 5),
-                range = 10,
-                position = new Vector3(0, 0, -5),
-                rotation = Quaternion.FromEulerAngles(0, 0, 0)
-            };
-
-            Game.activeLights[0] = light1;
-            //Game.activeLights[1] = light2;
-            Game.mainCamera = new Camera();
-            Game.mainCamera.Transform.position = new Vector3(0, 0, -5);
+            Game.activeLights[0] = light;
+            camera = new Camera();
+            camera.Transform.localPosition = new Vector3(0, 0, 0);
+            Game.mainCamera = camera;
             Input.MouseSensitivity = 0.1f;
+
+            cube = new GameObject
+            {
+                drawable = Drawable.MakeDrawable(Mesh.Load("assets/mesh/cube.dae"), Shaders.Lit),
+                transform = new Transform() { localPosition = new Vector3(0, 0, -5) }
+            };
+            monkey = new GameObject
+            {
+                drawable = Drawable.MakeDrawable(Mesh.Load("assets/mesh/monkey.dae"), Shaders.Lit),
+                transform = new Transform() { localPosition = new Vector3(4, 0, 0), Parent=cube.transform }
+            };
+            GameObject.Add(cube);
+            GameObject.Add(monkey);
+
         }
 
+        static float t;
         public override void Update(object sender, FrameEventArgs e)
         {
-            //drawable.rotation *= Matrix4.CreateFromQuaternion(Quaternion.FromEulerAngles(0, 0.01f, 0));
+            t += 0.01f;
             if(Input.GetKey(Key.W))
             {
-                Game.mainCamera.Transform.position += Game.mainCamera.Transform.Forward * 0.1f;
+                camera.Transform.localPosition += Game.mainCamera.Transform.Forward * 0.1f;
             }
             if (Input.GetKey(Key.S))
             {
-                Game.mainCamera.Transform.position -= Game.mainCamera.Transform.Forward * 0.1f;
+                camera.Transform.localPosition -= Game.mainCamera.Transform.Forward * 0.1f;
             }
             if (Input.GetKey(Key.A))
             {
-                Game.mainCamera.Transform.position -= Game.mainCamera.Transform.Right * 0.1f;
+                camera.Transform.localPosition -= Game.mainCamera.Transform.Right * 0.1f;
             }
             if (Input.GetKey(Key.D))
             {
-                Game.mainCamera.Transform.position += Game.mainCamera.Transform.Right * 0.1f;
+                camera.Transform.localPosition += Game.mainCamera.Transform.Right * 0.1f;
             }
             if (Input.GetKey(Key.Q))
             {
-                Game.mainCamera.Transform.Rotate(0, 0, -2);
+                camera.Transform.Rotate(0, 0, -2);
             }
             if (Input.GetKey(Key.E))
             {
-                Game.mainCamera.Transform.Rotate(0, 0, 2);
+                camera.Transform.Rotate(0, 0, 2);
             }
-            Game.mainCamera.Transform.Rotate(-Input.MouseDelta.Y * Input.MouseSensitivity, -Input.MouseDelta.X * Input.MouseSensitivity, 0);
+            camera.Transform.Rotate(-Input.MouseDelta.Y * Input.MouseSensitivity, -Input.MouseDelta.X * Input.MouseSensitivity, 0);
+            light.position = camera.Transform.Position;
+
+            cube.transform.Rotate(1, 1, 1);
         }
 
         public override void Render(object sender, FrameEventArgs e)
         {
-            drawable.Render(Game.mainCamera);
+
         }
     }
 }
