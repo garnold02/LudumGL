afc2445a0df5fb8280af9c64166d093f3ecbf652
Garnold02 <gulyas.arnold02@gmail.com>
2019-04-07 17:56:13 +0200

    Added materials

---
+++ LudumGL/Material.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OpenTK;
+namespace LudumGL
+{
+    /// <summary>
+    /// Material used for rendering.
+    /// </summary>
+    public class Material
+    {
+        #region Static
+        public static Material Default { get => new Material(); }
+        #endregion
+
+        public Texture Texture { get; set; }
+        public Vector4 Albedo { get; set; } = new Vector4(1, 1, 1, 1);
+        public Vector2 Tiling { get; set; } = new Vector2(1, 1);
+    }
+}
