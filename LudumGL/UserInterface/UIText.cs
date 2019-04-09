f785eea39a122ed68e2019648831f7643a6c4af4
Garnold02 <gulyas.arnold02@gmail.com>
2019-04-09 22:52:04 +0200

    Added text

---
+++ LudumGL/UserInterface/UIText.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using OpenTK;
+
+using LudumGL.Rendering;
+
+namespace LudumGL.UserInterface
+{
+    /// <summary>
+    /// UI element used for displaying text.
+    /// </summary>
+    public class UIText : UIElement
+    {
+        /// <summary>
+        /// The string this Text element will display.
+        /// </summary>
+        public string Text { get; set; }
+
+        public UIText()
+        {
+            DrawableText drawableText = new DrawableText();
+            drawableText.AddShader(Shaders.Projection);
+            drawableText.AddShader(Shaders.Unlit);
+            drawableText.Finish();
+
+            drawables.Add(drawableText);
+        }
+
+        public override void Update()
+        {
+            PositionAndScale();
+
+            DrawableText drawableText = (DrawableText)Drawable;
+            drawableText.Text = Text;
+            drawableText.Transform.localPosition = new Vector3(0, 0, -1);
+            drawableText.Transform.localScale = new Vector3(pixelSize.X, pixelSize.Y, 1);
+            drawableText.Material = Material;
+
+            base.Update();
+        }
+    }
+}
