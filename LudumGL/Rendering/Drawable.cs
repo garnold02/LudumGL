350a266ab7605ab44c2bb96122df795637da4535
Garnold02 <gulyas.arnold02@gmail.com>
2019-04-09 22:51:32 +0200

    Added namespace for clarity

---
+++ LudumGL/Rendering/Drawable.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace LudumGL.Rendering
+{
+    /// <summary>
+    /// All drawable things inherit from this class.
+    /// </summary>
+    public abstract class Drawable
+    {
+        internal int program;
+        internal List<int> shaders;
+        internal Dictionary<string, int> uniforms;
+
+        /// <summary>
+        /// The material of this object.
+        /// </summary>
+        public Material Material { get; set; } = Material.Default;
+
+        /// <summary>
+        /// The transform of this object.
+        /// </summary>
+        public Transform Transform { get; set; } = Transform.Identity;
+
+        /// <summary>
+        /// Specifies which matrices of the used camera should be ignored.
+        /// Useful for rendering skyboxes.
+        /// </summary>
+        public MatrixIgnoreMode CameraIgnore { get; set; } = MatrixIgnoreMode.None;
+
+        public Drawable()
+        {
+            shaders = new List<int>();
+            uniforms = new Dictionary<string, int>();
+
+            program = GL.CreateProgram();
+        }
+
+        /// <summary>
+        /// Attaches a shader to this object.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="type"></param>
+        public void AddShader(Shader shader)
+        {
+            int glShader = GL.CreateShader(shader.Type);
+            GL.ShaderSource(glShader, shader.Code);
+            GL.CompileShader(glShader);
+            shaders.Add(glShader);
+            string log = GL.GetShaderInfoLog(glShader);
+            if (log.Length > 0) Console.WriteLine("Shader: {0}", log);
+        }
+
+        /// <summary>
+        /// Finalizes this object. Always call before
+        /// trying to draw the object.
+        /// </summary>
+        public virtual void Finish()
+        {
+            int i = 0;
+            foreach (int shader in shaders)
+            {
+                GL.AttachShader(program, shader);
+                i++;
+            }
+            GL.LinkProgram(program);
+            foreach (int shader in shaders)
+            {
+                GL.DetachShader(program, shader);
+            }
+            shaders.Clear();
+        }
+
+        public abstract void Render(Camera camera);
+
+        #region UniformSetters
+        /// <summary>
+        /// Pass a float to the shader as a uniform property.
+        /// </summary>
+        /// <param name="name">Property name</param>
+        /// <param name="value"></param>
+        public void SetFloat(string name, float value)
+        {
+            if (!uniforms.ContainsKey(name))
+            {
+                int uniform = GL.GetUniformLocation(program, name);
+                uniforms.Add(name, uniform);
+
+            }
+            int location = uniforms[name];
+            GL.Uniform1(location, value);
+        }
+
+        /// <summary>
+        /// Pass a 2D vector to the shader as a uniform property.
+        /// </summary>
+        /// <param name="name">Property name</param>
+        /// <param name="value"></param>
+        public void SetVector2(string name, Vector2 value)
+        {
+            if (!uniforms.ContainsKey(name))
+            {
+                int uniform = GL.GetUniformLocation(program, name);
+                uniforms.Add(name, uniform);
+            }
+            int location = uniforms[name];
+            GL.Uniform2(location, ref value);
+        }
+
+        /// <summary>
+        /// Pass a 3D vector to the shader as a uniform property.
+        /// </summary>
+        /// <param name="name">Property name</param>
+        /// <param name="value"></param>
+        public void SetVector3(string name, Vector3 value)
+        {
+            if (!uniforms.ContainsKey(name))
+            {
+                int uniform = GL.GetUniformLocation(program, name);
+                uniforms.Add(name, uniform);
+            }
+            int location = uniforms[name];
+            GL.Uniform3(location, ref value);
+        }
+
+        /// <summary>
+        /// Pass a 4D vector to the shader as a uniform property.
+        /// </summary>
+        /// <param name="name">Property name</param>
+        /// <param name="value"></param>
+        public void SetVector4(string name, Vector4 value)
+        {
+            if (!uniforms.ContainsKey(name))
+            {
+                int uniform = GL.GetUniformLocation(program, name);
+                uniforms.Add(name, uniform);
+            }
+            int location = uniforms[name];
+            GL.Uniform4(location, ref value);
+        }
+
+        /// <summary>
+        /// Pass a 4x4 Matrix to the shader as a uniform property.
+        /// </summary>
+        /// <param name="name">Property name</param>
+        /// <param name="value"></param>
+        public void SetMatrix4(string name, Matrix4 value)
+        {
+            if (!uniforms.ContainsKey(name))
+            {
+                int uniform = GL.GetUniformLocation(program, name);
+                uniforms.Add(name, uniform);
+            }
+            int location = uniforms[name];
+            GL.UniformMatrix4(location, false, ref value);
+        }
+        #endregion
+    }
+
+    /// <summary>
+    /// Specifies which camera matrices the renderer
+    /// should ignore.
+    /// </summary>
+    public enum MatrixIgnoreMode
+    {
+        None = 0b0000,
+        Translation = 0b1000,
+        Rotation = 0b0100,
+        Scale = 0b0010,
+        Projection = 0b0001
+    }
+}
---
+++ LudumGL/Rendering/DrawableText.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace LudumGL.Rendering
+{
+    /// <summary>
+    /// A simple drawable object capable of rendering
+    /// text.
+    /// </summary>
+    public class DrawableText : Drawable
+    {
+        readonly int uvBuffer;
+
+        /// <summary>
+        /// The string this TextRenderer will render.
+        /// </summary>
+        public string Text { get; set; }
+
+        public DrawableText() : base()
+        {
+            uvBuffer = GL.GenBuffer();
+        }
+
+        public override void Render(Camera camera)
+        {
+            GL.UseProgram(program);
+
+            //Bind texture
+            if (Material.Texture != null)
+            {
+                GL.BindTexture(TextureTarget.Texture2D, Material.Texture.glTexture);
+                GL.ActiveTexture(TextureUnit.Texture0);
+                SetFloat("tex0", 0);
+                SetVector2("tiling", Material.Tiling);
+                SetFloat("useTex", 1);
+            }
+            else
+            {
+                SetFloat("useTex", 0);
+            }
+
+            for (int i = 0; i < Text.Length; i++)
+            {
+                RenderGlyph(camera, Text[i], new Vector3(Material.Texture.Width * i, 0, 0));
+            }
+
+            GL.BindTexture(TextureTarget.Texture2D, 0);
+        }
+
+        void RenderGlyph(Camera camera, char character, Vector3 position)
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, Mesh.Rectangle.vertexBuffer);
+            GL.EnableVertexAttribArray(0);
+            GL.VertexAttribPointer(0, 4, VertexAttribPointerType.Float, false, 0, 0);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, Mesh.Rectangle.normalBuffer);
+            GL.EnableVertexAttribArray(1);
+            GL.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 0, 0);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, uvBuffer);
+            GL.BufferData(BufferTarget.ArrayBuffer, Mesh.Rectangle.uvs.Length, Mesh.MapUvArray(Mesh.Rectangle.uvs, 0, 1, 0, 1), BufferUsageHint.DynamicDraw);
+            GL.EnableVertexAttribArray(2);
+            GL.VertexAttribPointer(2, 4, VertexAttribPointerType.Float, false, 0, 0);
+
+            SetMatrix4("translation", Transform.TranslationMatrix * Matrix4.CreateTranslation(position));
+            SetMatrix4("rotation", Transform.RotationMatrix);
+            SetMatrix4("scale", Transform.ScaleMatrix);
+
+            //Set camera matrices
+            {
+                Matrix4 cameraTranslation = Matrix4.Identity;
+                Matrix4 cameraRotation = Matrix4.Identity;
+                Matrix4 cameraProjection = Matrix4.Identity;
+                if (((int)CameraIgnore & 0b1000) == 0b0000)
+                    cameraTranslation = camera.Transform.TranslationMatrix;
+
+                if (((int)CameraIgnore & 0b0100) == 0b0000)
+                    cameraRotation = camera.Transform.RotationMatrix;
+                if (((int)CameraIgnore & 0b0001) == 0b0000)
+                    cameraProjection = camera.Projection;
+
+                SetMatrix4("projection", (Matrix4.Invert(cameraTranslation) * cameraRotation) * cameraProjection);
+            }
+
+            SetVector3("ambient", Game.AmbientLightColor);
+            SetVector4("albedo", Material.Albedo);
+
+            GL.DrawElements(PrimitiveType.Triangles, Mesh.Rectangle.vertices.Length, DrawElementsType.UnsignedInt, Mesh.Rectangle.indices);
+        }
+    }
+}
