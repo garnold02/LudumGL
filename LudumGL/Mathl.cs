ea3b159f22c4e32e919ea9693931bac8f0a9aa23
Garnold02 <gulyas.arnold02@gmail.com>
2019-04-09 22:52:30 +0200

    Added extra math functions

---
+++ LudumGL/Mathl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LudumGL
+{
+    /// <summary>
+    /// Class containing useful mathematics.
+    /// </summary>
+    class Mathl
+    {
+        /// <summary>
+        /// Maps a value from one range to another.
+        /// </summary>
+        /// <param name="value">The original value</param>
+        /// <param name="oldMin">Minimum of the original range</param>
+        /// <param name="oldMax">Maximum of the original range</param>
+        /// <param name="newMin">Minimum of the new range</param>
+        /// <param name="newMax">Maximum of the new range</param>
+        /// <returns>The new value</returns>
+        public static float Map(float value, float oldMin, float oldMax, float newMin, float newMax)
+        {
+            //First, normalize the value
+            float normalizedValue = (value - oldMin) / (oldMax - oldMin);
+
+            //Then, scale up the normalized value
+            float scaledValue = normalizedValue * (newMax - newMin);
+
+            //Finally, add the new minimum to the scaled value.
+            float finalValue = newMin + scaledValue;
+
+            //Return the final value
+            return finalValue;
+        }
+    }
+}
