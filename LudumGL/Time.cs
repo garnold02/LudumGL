ddc2ab83841b36748b9b13c746297f50e08ef4eb
Garnold02 <gulyas.arnold02@gmail.com>
2019-04-10 23:22:28 +0200

    Added deltaTime and elapsedTime

---
+++ LudumGL/Time.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LudumGL
+{
+    /// <summary>
+    /// Class for dealing with time.
+    /// </summary>
+    public class Time
+    {
+        /// <summary>
+        /// Gets the time spent in the last frame, in seconds.
+        /// </summary>
+        public static float DeltaTime { get => (float)Game.window.UpdateTime + (float)Game.window.RenderTime; }
+
+        /// <summary>
+        /// Gets the elapsed time since the start of the game, in seconds.
+        /// </summary>
+        public static float Elapsed { get => Game.elapsedTime; }
+    }
+}
