c716cab7eb0ebc750b4a024b29baa16c79a1b9ff
Garnold02 <gulyas.arnold02@gmail.com>
2019-04-12 22:20:22 +0200

    Started implementing scenes

---
+++ LudumGL/Scene/SceneName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LudumGL.Scene
+{
+    /// <summary>
+    /// Attribute used to specify the name of a field/property
+    /// used in scene saving and loading.
+    /// </summary>
+    class SceneName : Attribute
+    {
+        internal string name;
+        public SceneName(string name)
+        {
+            this.name = name;
+        }
+    }
+}
